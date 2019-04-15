edf7b5302cecd38c75df4414a303067566d3c8de
Randall Floyd <rdfloyd@indiana.edu>
2019-04-15 16:33:31 -0400

    Modifying search and indexing for blacklight_iiif_search

--- app/indexers/essi/file_set_indexer.rb
+++ app/indexers/essi/file_set_indexer.rb
@@ -1,5 +1,14 @@
 module ESSI
   class FileSetIndexer < Hyrax::FileSetIndexer
     include ESSI::IIIFThumbnailBehavior
+
+    def generate_solr_document
+      super.tap do |solr_doc|
+        solr_doc['is_page_of_ssi'] = object.parent_id
+        solr_doc['text_timv'] = object.extracted_text.content if object.extracted_text.present?
+      end
+    end
+
   end
 end
+
